48c241ae93af88cedfad0f9324ae9de9b188e372
Samuel Santos <80470130+samuel06santos@users.noreply.github.com>
2022-05-06 18:28:28 -0300

    Update 1035 - Teste de Seleção 1.py

--- 1035/1035 - Teste de Seleção 1.py
+++ 1035/1035 - Teste de Seleção 1.py
@@ -1,5 +1,4 @@
-x = input().split()
-a,b,c,d = x
+a,b,c,d = input().split()
 a,b,c,d = int(a),int(b),int(c),int(d)
 if b > c and d > a and (c + d) > (a + b) and c > 0 and d > 0 and a % 2 == 0:
     print("Valores aceitos") 
