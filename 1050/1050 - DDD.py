3e1abee8337b8a7c7eed57d87719619120f095a2
Samuel Santos <80470130+samuel06santos@users.noreply.github.com>
2022-05-12 13:53:05 -0300

    Create 1050 - DDD.py

---
+++ 1050/1050 - DDD.py
@@ -0,0 +1,11 @@
+x = int(input())
+if x == 61: ddd = 'Brasilia' 
+elif x == 71: ddd = 'Salvador' 
+elif x == 11: ddd = 'Sao Paulo' 
+elif x == 21: ddd = 'Rio de Janeiro' 
+elif x == 32: ddd = 'Juiz de Fora'
+elif x == 19: ddd = 'Campinas' 
+elif x == 27: ddd = 'Vitoria' 
+elif x == 31: ddd = 'Belo Horizonte' 
+else: ddd = 'DDD nao cadastrado'
+print(ddd)
