ecf30020c44433ef6a4bc5c51ce65bd6b604fe9b
Samuel Santos <80470130+samuel06santos@users.noreply.github.com>
2022-08-22 16:19:58 -0300

    Create 1003 - Soma Simples.cs

---
+++ 1003/1003 - Soma Simples.cs
@@ -0,0 +1,9 @@
+using System;
+class Program{
+    static void Main(){
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int soma = a + b;
+        Console.WriteLine("SOMA = " + soma);
+    }
+}
