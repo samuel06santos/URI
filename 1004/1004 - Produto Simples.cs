992ae3731aaf203c63b85b19a50683ca69efa7b9
Samuel Santos <80470130+samuel06santos@users.noreply.github.com>
2022-09-03 12:16:46 -0300

    Create 1004 - Produto Simples.cs

---
+++ 1004/1004 - Produto Simples.cs
@@ -0,0 +1,9 @@
+using System;
+class Program{
+    static void Main(){
+        int v1,v2;
+        v1 = Convert.ToInt16(Console.ReadLine());
+        v2 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine($"PROD = {v1*v2}");
+    }
+}
