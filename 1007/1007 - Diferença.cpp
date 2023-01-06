3f24a19f4a0f1347fc16cf4761bffdc4f7028353
Samuel Santos <80470130+samuel06santos@users.noreply.github.com>
2023-01-06 19:39:50 -0300

    Create 1007 - Diferença.cpp

---
+++ 1007/1007 - Diferença.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+    int a, b, c, d;
+    std::cin >> a; std::cin >> b; std::cin >> c; std::cin >> d;
+    std::cout << "DIFERENCA = " << (a*b)-(c*d) <<std::endl;
+    return 0;
+}
