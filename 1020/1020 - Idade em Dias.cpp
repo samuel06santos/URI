c82e8d7b8a86fa7ddcec3018c8856e68765d844f
Samuel Santos <80470130+samuel06santos@users.noreply.github.com>
2023-01-26 01:18:34 -0300

    Update 1020 - Idade em Dias.cpp

--- 1020/1020 - Idade em Dias.cpp
+++ 1020/1020 - Idade em Dias.cpp
@@ -4,7 +4,7 @@ int main() {
     std::cin >> age;
     printf("%d ano(s)\n", age/365);
     age %= 365;
-    printf("%d mes(s)\n", age/30);
+    printf("%d mes(es)\n", age/30);
     age %= 30;
     printf("%d dias(s)\n", age);
     return 0;
