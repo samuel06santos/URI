11a59d9e4b7e9d7dba689285c41c873111603880
Samuel Santos <80470130+samuel06santos@users.noreply.github.com>
2023-11-04 00:04:26 -0300

    Update 1007 - Diferença.ex

--- 1007/1007 - Diferença.ex
+++ 1007/1007 - Diferença.ex
@@ -1,6 +1,6 @@
-{a,_} = IO.read(:stdio,:line) |> Integer.parse
-{b,_} = IO.read(:stdio,:line) |> Integer.parse
-{c,_} = IO.read(:stdio,:line) |> Integer.parse
-{d,_} = IO.read(:stdio,:line) |> Integer.parse
-r = (a*b)-(c*d)
-IO.puts("DIFERENCA = #{r}")
+{a, _} = IO.read(:stdio,:line) |> Integer.parse
+{b, _} = IO.read(:stdio,:line) |> Integer.parse
+{c, _} = IO.read(:stdio,:line) |> Integer.parse
+{d, _} = IO.read(:stdio,:line) |> Integer.parse
+
+IO.puts("DIFERENCA = #{(a*b) - (c*d)}")
